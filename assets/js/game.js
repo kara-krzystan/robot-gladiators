fe4aacae0b96e371a5e42fbdf3a0ecfab54def94
Kara Krzystan <kara.krzystan@gmail.com>
2020-10-08 13:22:20 -0700

    Added conditions for checking robot health

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,35 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-  window.alert("The fight has begun!")
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+
+enemyHealth = enemyHealth - playerAttack;
+
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. ");
+
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died! ");
+}
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
 }
 
-// fight();
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died ");
+}
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+}
+
+};
+
+fight();
